cc9576bd436c9007c599ae61aa73bea1028157ba
Swapnil Pande <swapnil.pande98@gmail.com>
2019-02-26 09:27:58 -0600

    WIP #8 #10: Added ExperimentInterpreter documentation

--- ExperimentalFramework/ExperimentInterpreter.py
+++ ExperimentalFramework/ExperimentInterpreter.py
@@ -4,8 +4,16 @@ import os
 import docker_plugin
 import slurm_plugin
 
+## The ExperimentIntrepeter class is the main handler for the Experiment
+# The interpreter takes in an experiment config JSON and generates an experiment
+# Additionally, it interfaces with all of the artifact generation plugins to generate all of the experiment artifacts
+# It then will execute the experiment by sending the jobs to the SLURM workload manager
+# A single instance of the ExperimentInterpreter should be instantiated by the application server
 class ExperimentInterpreter:
-
+    ## __init__(self, artifactDir, mlFwk)
+    # artifactDir - Directory in which to generate all of the experiment artifacts
+    # mlFwk - The experimental framework being used for the experiments
+    #           Currently, the Interpreter object stores the framework to avoid importing all of the framework plugins
     def __init__(self, artifactDir, mlFwk):
         # Save the directory in which all of the artifacts for the experiments will be generated
         self.artifactDir = artifactDir
@@ -21,7 +29,10 @@ class ExperimentInterpreter:
         self.dockerPlugin = docker_plugin.DockerPlugin()
         self.slurmPlugin = slurm_plugin.SlurmPlugin()
 
-
+    ## executeExperiment(self, expConfigFilepath)
+    # This method should be called to run an experiment from a config JSON file
+    # The method will instantiate the experiment object, generate all of the training artifacts, and send the jobs to SLURM
+    # expConfigFilepath - Path to the config JSON
     def executeExperiment(self, expConfigFilepath):
         # Open and read in JSON file as dictionary
         with open(expConfigFilepath) as f:
@@ -33,16 +44,17 @@ class ExperimentInterpreter:
         # Generate directories for experiment and experiment instances in the artifact directory
         self.__createExperimentDirs__(experiment)
 
+        # Generate artifacts
         self.generateTrainingFiles(experiment)
         self.generateDockerFiles(experiment)
         self.generateSlurmBatchFile(experiment)
 
 
+    ## __generateExperimentObject__(self, expConfig)
+    # Generates an experiment object given a experiment config
+    # expConfig - Dictionary containing contents of config JSON file
     def __generateExperimentObject__(self, expConfig):
-
         # Generate a dictionary containing slurm configuration from config file
-        # Does not include the output parameter
-
         expDir = self.__generateUniqueExperimentDir__(expConfig["label"])
 
         #Check if experiment type is static simple. If it is, create an experiment object and generate the instances
