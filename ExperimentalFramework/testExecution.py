b32400dc5dfa3bc01c51c2b97be828bee4d74d2a
Swapnil Pande <swapnil.pande98@gmail.com>
2019-03-13 21:02:52 -0500

    Delete testExecution.py

--- ExperimentalFramework/testExecution.py
+++
@@ -1,4 +0,0 @@
-from ExperimentInterpreter import ExperimentInterpreter
-
-expInterpret = ExperimentInterpreter("C:\\Users\\Swapnil\\Desktop\\ImportantThings\\MaxMobility\\ExperimentFramework\\artifacts", "keras")
-expInterpret.executeExperiment("C:\\Users\\Swapnil\\Desktop\\ImportantThings\\MaxMobility\\ExperimentFramework\\ml-experimental-framework\\Samples\\MNIST", "MNISTExperiment.json")
